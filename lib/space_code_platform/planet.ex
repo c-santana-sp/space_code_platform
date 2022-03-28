89fbcc961c8f58c2cfa02c152ac44d0940523755
c-santana-sp <carlos.santana@bandtec.com.br>
2022-03-28 00:18:32 -0300

    feat: adding one_to_many Route relation in Planet

--- lib/space_code_platform/planet.ex
+++ lib/space_code_platform/planet.ex
@@ -2,10 +2,13 @@ defmodule SpaceCodePlatform.Planet do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias SpaceCodePlatform.Route
+
   @primary_key {:id, Ecto.UUID, autogenerate: true}
   @required_fields [:name]
   schema "planets" do
     field :name, :string
+    has_many :routes, Route
     timestamps()
   end
 
