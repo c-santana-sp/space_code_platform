59280062bcd26d9bd6988d668e38a97a68f9d8a8
c-santana-sp <carlos.santana@bandtec.com.br>
2022-03-28 00:09:45 -0300

    feat: adding route schema

---
+++ lib/space_code_platform/route.ex
@@ -0,0 +1,30 @@
+defmodule SpaceCodePlatform.Route do
+
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias SpaceCodePlatform.Planet
+
+  @primary_key {:id, Ecto.UUID, autogenerate: true}
+  @foreign_key_type Ecto.UUID
+  @required_fields [:fuel_cost, :is_available, :origin_planet_id, :destination_planet_id]
+  schema "routes" do
+    field :fuel_cost, :float
+    field :is_available, :boolean
+    belongs_to :origin_planet, Planet, references: :id
+    belongs_to :destination_planet, Planet, references: :id
+    timestamps()
+  end
+
+  def build(params) do
+    params
+    |> changeset()
+    |> apply_action(:insert)
+  end
+
+  def changeset(params) do
+    %__MODULE__{}
+    |> cast(params, @required_fields)
+    |> validate_required(@required_fields)
+  end
+end
