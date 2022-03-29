d19216442c024a10613471bb9d3cc4fdbf3b034c
c-santana-sp <carlos.santana@bandtec.com.br>
2022-03-29 15:32:17 -0300

    feat: adding create view in PilotsView

--- lib/space_code_platform_web/views/pilots_view.ex
+++ lib/space_code_platform_web/views/pilots_view.ex
@@ -3,14 +3,35 @@ defmodule SpaceCodePlatformWeb.PilotsView do
 
   alias SpaceCodePlatform.Pilot
 
+  def render("create.json", %{pilot: %Pilot{
+    id: id,
+    name: name,
+    pilot_certification: pilot_certification,
+    credits: credits,
+    age: age,
+    location_planet_id: location_planet_id,
+    inserted_at: inserted_at,
+    updated_at: updated_at}}) do
+    %{
+      id: id,
+      name: name,
+      pilot_certification: pilot_certification,
+      credits: credits,
+      age: age,
+      location_planet_id: location_planet_id,
+      inserted_at: inserted_at,
+      updated_at: updated_at
+    }
+  end
+
   def render("fetch.json", %{pilot: %Pilot{
     id: id,
     name: name,
     pilot_certification: pilot_certification,
     age: age,
     credits: credits,
-    location_planet: location_planet,
-    ship: ship,
+    location_planet_id: location_planet_id,
+    # ship: ship,
     inserted_at: inserted_at,
     updated_at: updated_at}}) do
     %{
@@ -19,8 +40,8 @@ defmodule SpaceCodePlatformWeb.PilotsView do
       pilot_certification: pilot_certification,
       age: age,
       credits: credits,
-      location_planet: location_planet,
-      ship: ship,
+      location_planet_id: location_planet_id,
+      # ship: ship,
       inserted_at: inserted_at,
       updated_at: updated_at
     }
